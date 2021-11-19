3fff291db1f92980225b1a0730195320aa642c76
Carlos Saenz <carlos.saenz.g99@gmail.com>
2021-11-18 21:40:43 -0500

    chore: adminpro

--- angular/Admin/src/app/app.module.ts
+++ angular/Admin/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
