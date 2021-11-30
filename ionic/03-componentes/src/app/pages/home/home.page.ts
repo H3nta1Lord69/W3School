e25c7460a58cdd41182cbfea53da934274f2bfd6
Carlos Saenz <carlos.saenz.g99@gmail.com>
2021-11-29 23:51:56 -0500

    chore: Ionic Ion-list

--- ionic/03-componentes/src/app/pages/home/home.page.ts
+++ ionic/03-componentes/src/app/pages/home/home.page.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Components {
+
+  icon: string;
+  name: string;
+  redirectTo: string;
+
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  components: Components[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
 
   constructor() { }
 
