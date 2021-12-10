c834c40c8210453ff75fdaf6f1ca2a07f515e878
Carlos Saenz <carlos.saenz.g99@gmail.com>
2021-12-09 19:26:43 -0500

    chore: News App - header manipulation

--- ionic/04-news/src/environments/environment.prod.ts
+++ ionic/04-news/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '2e66c6d412e34fbe997a0fd645402b6d'
 };
--- ionic/04-news/src/environments/environment.ts
+++ ionic/04-news/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '2e66c6d412e34fbe997a0fd645402b6d'
 };
 
 /*
