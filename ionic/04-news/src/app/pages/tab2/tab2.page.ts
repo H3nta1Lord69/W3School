a7ea58d9097edf180aa5a9487abd46479808bee5
Carlos Saenz <carlos.saenz.g99@gmail.com>
2021-12-11 14:26:13 -0500

    chore: Ionic - News Categories

--- ionic/04-news/src/app/pages/tab2/tab2.page.ts
+++ ionic/04-news/src/app/pages/tab2/tab2.page.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { Article } from '../interfaces';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   public categories: string[] = [
+    'general',
     'business',
     'entertainment',
-    'general',
     'health',
     'science',
     'sports',
@@ -19,10 +21,26 @@ export class Tab2Page {
 
   public selectedCategory: string = this.categories[0];
 
-  constructor() {}
+  public articles: Article[] = [];
 
-  segmentChanged( ev ) {
-    console.log(ev);
+  constructor( private newsService: NewsService ) {}
+
+  ngOnInit() {
+    
+    this.newsService.getTopHeadlinesByCategory(this.selectedCategory)
+      .subscribe( articles => {
+        this.articles = [ ...this.articles, ...articles ];
+    });
+
+  }
+
+  segmentChanged( ev: any ) {
+    this.selectedCategory = ev.detail.value;
+    console.log(ev.detail.value);
+    this.newsService.getTopHeadlinesByCategory(this.selectedCategory)
+      .subscribe( articles => {
+        this.articles = [ ...articles ];
+    });
   }
 
 }
