bf248f6abc5fcb05fd4fd9a9c1c54f0c03f71e34
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2021-12-28 20:53:47 -0500

    chore: Showing the images - MoviesApp

--- ionic/05-peliculasApp/src/app/pipes/imagen.pipe.ts
+++ ionic/05-peliculasApp/src/app/pipes/imagen.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const URL = 'https://image.tmdb.org/t/p';
+
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(img: string, size: string = 'w500'): string {
+    if(!img) {
+      return './assets/no-image-banner.jpg';
+    }
+
+    const imgUrl = `${URL}/${size}${img}`;
+
+    return imgUrl;
   }
 
 }
