82e11eed8db1c726d7973bb3bf300bd5816675f9
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-01-02 21:17:36 -0500

    chore: Global variables environment

--- ionic/05-peliculasApp/src/environments/environment.prod.ts
+++ ionic/05-peliculasApp/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'bff5f0b9426eedd4103be48faf24d408',
+  imgPath: 'https://image.tmdb.org/t/p',
 };
--- ionic/05-peliculasApp/src/environments/environment.ts
+++ ionic/05-peliculasApp/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'bff5f0b9426eedd4103be48faf24d408',
+  imgPath: 'https://image.tmdb.org/t/p',
 };
 
 /*
