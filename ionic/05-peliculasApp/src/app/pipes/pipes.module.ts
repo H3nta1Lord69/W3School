3047bf0cfa22b41c5eac9304c07fc05df0e62b2a
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-01-04 00:12:28 -0500

    chore: Pairs pipe module

--- ionic/05-peliculasApp/src/app/pipes/pipes.module.ts
+++ ionic/05-peliculasApp/src/app/pipes/pipes.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagenPipe } from './imagen.pipe';
-
-
+import { PairsPipe } from './pairs.pipe';
 
 @NgModule({
-  declarations: [
-    ImagenPipe
-  ],
-  exports: [
-    ImagenPipe
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ImagenPipe, PairsPipe],
+  exports: [ImagenPipe, PairsPipe],
+  imports: [CommonModule],
 })
-export class PipesModule { }
+export class PipesModule {}
