12183d73e5158865154ee3753f177015990d2d58
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-01-04 22:56:43 -0500

    chore: Movie details component generated

--- ionic/05-peliculasApp/src/app/components/components.module.ts
+++ ionic/05-peliculasApp/src/app/components/components.module.ts
@@ -5,17 +5,20 @@ import { SlideshowBackdropComponent } from './slideshow-backdrop/slideshow-backd
 import { PipesModule } from '../pipes/pipes.module';
 import { SlideshowPosterComponent } from './slideshow-poster/slideshow-poster.component';
 import { SlideshowPairsComponent } from './slideshow-pairs/slideshow-pairs.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     SlideshowBackdropComponent,
     SlideshowPosterComponent,
     SlideshowPairsComponent,
+    MovieDetailsComponent,
   ],
   exports: [
     SlideshowBackdropComponent,
     SlideshowPosterComponent,
     SlideshowPairsComponent,
+    MovieDetailsComponent,
   ],
   imports: [CommonModule, IonicModule, PipesModule],
 })
