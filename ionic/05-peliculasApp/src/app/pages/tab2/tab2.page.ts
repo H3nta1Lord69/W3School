a0bf6220946de582504f1b22d87c13324ddad973
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-01-08 21:04:56 -0500

    chore: search page - ts logic

--- ionic/05-peliculasApp/src/app/pages/tab2/tab2.page.ts
+++ ionic/05-peliculasApp/src/app/pages/tab2/tab2.page.ts
@@ -3,10 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  searchText = '';
+  ideas: string[] = [
+    'Encanto',
+    'Avengers',
+    'Spiderman',
+    'El señor de los anillos',
+    'Batman',
+    'Superman',
+  ];
 
   constructor() {}
 
+  search(ev) {
+    const valueInput = ev.detail.value;
+    console.log(valueInput);
+  }
 }
