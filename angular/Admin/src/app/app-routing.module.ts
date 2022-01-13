2e1e57e476090bf500abc407e27049512946bd7f
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-13 14:06:57 -0500

    chore: Update routes in main routing file

--- angular/Admin/src/app/app-routing.module.ts
+++ angular/Admin/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
+// Angular libraries imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+// Routing modules
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent},
-
   { path: '**', component: NopagefoundComponent}
 ];
 
@@ -18,7 +17,8 @@ const routes: Routes = [
   declarations: [],
   imports: [
     RouterModule.forRoot( routes ),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
