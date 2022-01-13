d286c315df787c80f3a176e0b2421f8ed4c16db7
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-13 13:47:48 -0500

    chore: Improving routing system

--- angular/Admin/src/app/app-routing.module.ts
+++ angular/Admin/src/app/app-routing.module.ts
@@ -1,25 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { FirstGraphicComponent } from './pages/first-graphic/first-graphic.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent},
-      { path: 'progress', component: ProgressComponent},
-      { path: 'first_graphic', component: FirstGraphicComponent},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ]
-  },
-
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
 
@@ -30,7 +17,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule
   ],
   exports: [RouterModule]
 })
--- angular/Admin/src/app/pages/pages.routing.ts
+++ angular/Admin/src/app/pages/pages.routing.ts
@@ -1,7 +1,22 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { FirstGraphicComponent } from './first-graphic/first-graphic.component';
 
 const routes: Routes = [
+    
+    { 
+        path: '', 
+        component: PagesComponent,
+        children: [
+          { path: 'dashboard', component: DashboardComponent},
+          { path: 'progress', component: ProgressComponent},
+          { path: 'first_graphic', component: FirstGraphicComponent},
+          { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+        ]
+    },
     //{ path: 'path/:routeParam', component: MyComponent },
     //{ path: 'staticPath', component: ... },
     //{ path: '**', component: ... },
