46b8641aab84212c41361367fae7152d8e2fda57
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-17 12:09:47 -0500

    chore: Update pages.module including reactive forms in the imports

--- angular/Admin/src/app/pages/pages.module.ts
+++ angular/Admin/src/app/pages/pages.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { FirstGraphicComponent } from './first-graphic/first-graphic.component';
@@ -7,25 +9,19 @@ import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
 
-
-
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressComponent,
     FirstGraphicComponent,
-    PagesComponent
+    PagesComponent,
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     FirstGraphicComponent,
-    PagesComponent
-  ],  
-  imports: [
-    CommonModule,
-    SharedModule,
-    AppRoutingModule
-  ]
+    PagesComponent,
+  ],
+  imports: [CommonModule, FormsModule, SharedModule, AppRoutingModule],
 })
-export class PagesModule { }
+export class PagesModule {}
