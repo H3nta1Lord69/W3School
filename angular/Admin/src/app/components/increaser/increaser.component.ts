e3ee3685955fd21180e8e087318bf75ae6371330
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-18 19:19:13 -0500

    chore: Emitting the value with the output decorator

--- angular/Admin/src/app/components/increaser/increaser.component.ts
+++ angular/Admin/src/app/components/increaser/increaser.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-increaser',
@@ -8,15 +8,20 @@ import { Component, Input } from '@angular/core';
 export class IncreaserComponent {
   @Input('value') progress: number = 70;
 
+  @Output() valueOutput: EventEmitter<number> = new EventEmitter();
+
   changeValue(value: number) {
     if (this.progress >= 100 && value >= 0) {
+      this.valueOutput.emit(100);
       return (this.progress = 100);
     }
 
     if (this.progress <= 0 && value < 0) {
+      this.valueOutput.emit(0);
       return (this.progress = 0);
     }
 
+    this.valueOutput.emit(this.progress);
     return (this.progress = this.progress + value);
   }
 }
