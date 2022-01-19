38f0e0a27c1391023c03cd193752347315fc3f67
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-18 19:18:46 -0500

    chore: Main functions to receive the input from the components

--- angular/Admin/src/app/pages/progress/progress.component.ts
+++ angular/Admin/src/app/pages/progress/progress.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css'],
 })
-export class ProgressComponent {}
+export class ProgressComponent {
+
+  f_progress: number = 25;
+  s_progress: number = 35;
+
+  get getFirstProgress() {
+    return `${ this.f_progress }%`;
+  }
+
+  get getSecondProgress() {
+    return `${ this.s_progress }%`;
+  }
+
+  changeChildComponent( ev: number ) {
+    console.log("Hey!!!!!", ev);
+  }
+
+}
