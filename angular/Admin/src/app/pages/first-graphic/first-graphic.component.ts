86e440df4acd54f110a2660404d5bf7e74200aa6
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-19 21:25:24 -0500

    chore: Adding chart in the graphic component

--- angular/Admin/src/app/pages/first-graphic/first-graphic.component.ts
+++ angular/Admin/src/app/pages/first-graphic/first-graphic.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { ChartData, Color } from 'chart.js';
 
 @Component({
   selector: 'app-first-graphic',
@@ -6,11 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class FirstGraphicComponent implements OnInit {
+export class FirstGraphicComponent {
 
-  constructor() { }
+  // constructor() { }
 
-  ngOnInit(): void {
+  // ngOnInit(): void {
+  // }
+
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ] }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public colors: Color[] = [
+    //{ backgroundColor: [ '#6857E6', '#009FEE', '#F02059' ] }
+  ];
 
 }
