cda535085d0df04140359f6ee353de2fdfa518c5
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-21 13:27:25 -0500

    chore: Updating routes

--- angular/Admin/src/app/pages/pages.routing.ts
+++ angular/Admin/src/app/pages/pages.routing.ts
@@ -1,30 +1,33 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+
+// Component routes
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { FirstGraphicComponent } from './first-graphic/first-graphic.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 const routes: Routes = [
-    
-    { 
-        path: 'dashboard', 
-        component: PagesComponent,
-        children: [
-          { path: '', component: DashboardComponent},
-          { path: 'progress', component: ProgressComponent},
-          { path: 'first_graphic', component: FirstGraphicComponent},
-        ]
-    },
-    //{ path: 'path/:routeParam', component: MyComponent },
-    //{ path: 'staticPath', component: ... },
-    //{ path: '**', component: ... },
-    //{ path: 'oldPath', redirectTo: '/staticPath' },
-    //{ path: ..., component: ..., data: { message: 'Custom' }
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'first-graphic', component: FirstGraphicComponent },
+      { path: 'account-settings', component: AccountSettingsComponent },
+    ],
+  },
+  //{ path: 'path/:routeParam', component: MyComponent },
+  //{ path: 'staticPath', component: ... },
+  //{ path: '**', component: ... },
+  //{ path: 'oldPath', redirectTo: '/staticPath' },
+  //{ path: ..., component: ..., data: { message: 'Custom' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class PagesRoutingModule {}
