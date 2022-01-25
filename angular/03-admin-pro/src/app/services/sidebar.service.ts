f16306f5ba8cdb2a5a995242149d0ac209e9c877
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-01-25 00:08:43 -0500

    chore: Control options in the main sidebar

--- angular/03-admin-pro/src/app/services/sidebar.service.ts
+++ angular/03-admin-pro/src/app/services/sidebar.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SidebarService {
+  menu: Object[] = [
+    {
+      title: 'Dashboard',
+      icon: 'mdi mdi-gauge',
+      submenu: [
+        {
+          title: 'Main',
+          url: '/',
+        },
+        {
+          title: 'Progress Bar',
+          url: 'progress',
+        },
+        {
+          title: 'Graphics',
+          url: 'first-graphic',
+        },
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
