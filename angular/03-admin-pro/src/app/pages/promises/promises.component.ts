81b03293e9536f55ecbafd6fa35b5b64b4c14ddb
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-25 15:35:02 -0500

    chore: Getting API within a promise

--- angular/03-admin-pro/src/app/pages/promises/promises.component.ts
+++ angular/03-admin-pro/src/app/pages/promises/promises.component.ts
@@ -8,21 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class PromisesComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    const promise = new Promise((resolve, reject) => {
-      if (true) {
-        resolve('Holiwi');
-      } else {
-        reject('Algo anda mal');
-      }
+  ngOnInit() {
+    this.getUsers().then((users) => console.log(users));
+    //   const promise = new Promise((resolve, reject) => {
+    //     if (true) {
+    //       resolve('Holiwi');
+    //     } else {
+    //       reject('Algo anda mal');
+    //     }
+    //   });
+    //   promise
+    //     .then((msg) => {
+    //       console.log(msg, 'Finitto');
+    //     })
+    //     .catch((error) => console.log(`Somenthin's not right`));
+    //   console.log('Fin del init');
+  }
+
+  getUsers() {
+    return new Promise((resolve) => {
+      fetch('https://reqres.in/api/users')
+        .then((resp) => resp.json())
+        .then((body) => resolve(body.data))
+        .catch((err) => {
+          console.log(err);
+        });
     });
-
-    promise
-      .then((msg) => {
-        console.log(msg, 'Finitto');
-      })
-      .catch((error) => console.log(`Somenthin's not right`));
-
-    console.log('Fin del init');
   }
 }
