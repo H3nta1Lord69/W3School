7ab1708d9ce7eb6b7eb29867541485a61894a75f
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-27 20:56:24 -0500

    chore: Basic observable

--- angular/03-admin-pro/src/app/pages/rxjs/rxjs.component.ts
+++ angular/03-admin-pro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.css']
+  styleUrls: ['./rxjs.component.css'],
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent {
+  constructor() {
+    const obs$ = new Observable((observer) => {
+      let i = -1;
+      const interval = setInterval(() => {
+        i++;
+        observer.next(i);
 
-  constructor() { }
+        if (i === 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
 
-  ngOnInit(): void {
+        if (i === 2) {
+          observer.error('Reach 2');
+        }
+      }, 1000);
+    });
+
+    obs$.subscribe(
+      (value) => console.log('Subs:', value),
+      (err) => console.warn('Error:', err),
+      () => console.info('Obs terminado')
+    );
   }
-
 }
