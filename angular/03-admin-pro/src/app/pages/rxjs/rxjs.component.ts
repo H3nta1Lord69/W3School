7da0683da9b19f4564c56933695726e4eb8604ff
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-01-28 14:02:05 -0500

    chore: unsubscribe method

--- angular/03-admin-pro/src/app/pages/rxjs/rxjs.component.ts
+++ angular/03-admin-pro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 import { retry, take, map, filter } from 'rxjs/operators';
 
 @Component({
@@ -7,7 +7,9 @@ import { retry, take, map, filter } from 'rxjs/operators';
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css'],
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+  public intervalSubs: Subscription;
+
   constructor() {
     // this.returnObservable()
     //   .pipe(retry())
@@ -17,16 +19,19 @@ export class RxjsComponent {
     //     () => console.info('Obs terminado')
     //   );
 
-    this.returnInterval().subscribe(console.log);
+    this.intervalSubs = this.returnInterval().subscribe(console.log);
+  }
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
   }
 
   returnInterval(): Observable<number> {
-    return interval(500).pipe(
+    return interval(100).pipe(
       map((value) => {
         return value + 1;
       }),
-      filter((value) => (value % 2 === 0 ? true : false)),
-      take(20)
+      filter((value) => (value % 2 === 0 ? true : false))
+      // take(20),
     );
   }
 
