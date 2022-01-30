bc1a4e71e861703264ec7ebbf92ddf99bbd8081d
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-01-29 20:20:12 -0500

    chore: starting a server

--- node/01-backend-server/index.js
+++ node/01-backend-server/index.js
@@ -1,1 +1,9 @@
-console.log('Hello there');
+const express = require('express');
+
+// Create server
+const app = express();
+
+// Server running on 8080 port
+app.listen(8080, () => {
+    console.log('Server running on port ' + 8080);
+});
