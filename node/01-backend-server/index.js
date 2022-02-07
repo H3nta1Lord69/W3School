bf77b2a0ad49e14f0e8ce1dc23498f139d21305d
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-02-07 12:18:11 -0500

    chore: getting a response in the backend

--- node/01-backend-server/index.js
+++ node/01-backend-server/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 // Create server
 const app = express();
 
+// Routes
+app.get('/', (req, res) => {
+    res.status(200).json({
+        ok:true,
+        msg: 'Holiwi'
+    })
+})
+
 // Server running on 8080 port
 app.listen(8080, () => {
     console.log('Server running on port ' + 8080);
