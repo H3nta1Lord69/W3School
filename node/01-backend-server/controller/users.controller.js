04d18a481a4a054001353c24fe8b1b65f5a4ba14
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-02-16 23:56:13 -0500

    chore: email validation

--- node/01-backend-server/controller/users.controller.js
+++ node/01-backend-server/controller/users.controller.js
@@ -11,15 +11,33 @@ const getUsers = async (req, res) => {
 
 const createUsers = async (req, res) => {
   const { name, password, email } = req.body;
-  const user = new User(req.body);
 
-  // Save in the db
-  await user.save();
+  try {
+    const emailExists = await User.findOne({ email });
 
-  res.status(200).json({
-    ok: true,
-    user,
-  });
+    if (emailExists) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The email already exists",
+      });
+    }
+
+    const user = new User(req.body);
+
+    // Save in the db
+    await user.save();
+
+    res.status(200).json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Unexpected error...",
+    });
+  }
 };
 
 module.exports = { getUsers, createUsers };
