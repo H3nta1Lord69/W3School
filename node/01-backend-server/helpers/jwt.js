444dbe2234113c73d70d0a7216049316a5c1bf69
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-02-20 19:49:28 -0500

    chore: generating JWT

---
+++ node/01-backend-server/helpers/jwt.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (uid) => {
+  return new Promise(() => {
+    const payload = {
+      uid,
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "8h",
+      },
+      (err, token) => {
+        if (err) {
+          console.log(err);
+          reject(err, `Can't generate JWT`);
+        } else {
+          resolve(token);
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+  generateJWT,
+};
