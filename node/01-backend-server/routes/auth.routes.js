9f1d8666990c17dde3d3eb9e9a91e5fafdc0dfc7
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-02-20 19:25:50 -0500

    chore: adding auth controller

--- node/01-backend-server/routes/auth.routes.js
+++ node/01-backend-server/routes/auth.routes.js
@@ -2,7 +2,20 @@
  * Route: api/login
  */
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { login } = require("../controller/auth.controller");
+const { validateFields } = require("../middlewares/validate-fields");
 
 const router = Router();
+
+router.post(
+  "/",
+  [
+    check("email", "Email is mandatory").isEmail(),
+    check("password", "Password is mandatory").not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
 
 module.exports = router;
