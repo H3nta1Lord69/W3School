431f16b0e66e71b77af6a12723071492c63f73be
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-02-26 23:04:44 -0500

    chore: including express-fileupload library

--- node/01-backend-server/controller/uploads.controller.js
+++ node/01-backend-server/controller/uploads.controller.js
@@ -1,4 +1,16 @@
 const fileUpload = (req, res) => {
+  const type = req.params.type;
+  const id = req.params.id;
+
+  const validateType = ["hospitals", "doctors", "users"];
+
+  if (!validateType.includes(type)) {
+    return res.status(400).json({
+      ok: true,
+      msg: "Invalid type",
+    });
+  }
+
   res.status(200).json({
     ok: true,
     msg: "file uploaded",
