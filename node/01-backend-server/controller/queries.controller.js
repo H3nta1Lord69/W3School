20047d8a4b5f5ee8f9d4e7eb670d03f5cd2b767b
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-02-26 20:06:30 -0500

    chore: endpoint for general search

--- node/01-backend-server/controller/queries.controller.js
+++ node/01-backend-server/controller/queries.controller.js
@@ -1,11 +1,28 @@
-const getAllData = (req, res) => {
+const User = require("../models/users");
+const Hospital = require("../models/hospital");
+const Doctor = require("../models/doctor");
+
+const getAllData = async (req, res) => {
   const search = req.params.search;
-  console.log(search);
+  const regexp = new RegExp(search, "i");
+
+  const [users, hospitals, doctors] = await Promise.all([
+    User.find({
+      name: regexp,
+    }),
+    Hospital.find({
+      name: regexp,
+    }),
+    Doctor.find({
+      name: regexp,
+    }),
+  ]);
 
   res.status(200).json({
     ok: true,
-    msg: "Hi there",
-    search,
+    users,
+    hospitals,
+    doctors,
   });
 };
 
