480f2ad1671c94e84d5b1f6fae29fe08991a18cc
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-02-26 23:10:20 -0500

    chore: validating if a file exists

--- node/01-backend-server/controller/uploads.controller.js
+++ node/01-backend-server/controller/uploads.controller.js
@@ -11,6 +11,16 @@ const fileUpload = (req, res) => {
     });
   }
 
+  // Exist file
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({
+      ok: true,
+      msg: "No files were uploaded.",
+    });
+  }
+
+  // Process image
+
   res.status(200).json({
     ok: true,
     msg: "file uploaded",
--- node/01-backend-server/routes/upload.routes.js
+++ node/01-backend-server/routes/upload.routes.js
@@ -3,11 +3,15 @@
  */
 
 const { Router } = require("express");
+const expressFU = require("express-fileupload");
+
 const { fileUpload } = require("../controller/uploads.controller");
 
 const { validateJwt } = require("../middlewares/validate-jwt");
 
 const router = Router();
+
+router.use(expressFU());
 
 router.put("/:type/:id", validateJwt, fileUpload);
 
