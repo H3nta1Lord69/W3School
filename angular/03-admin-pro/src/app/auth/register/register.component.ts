11d491bd2f4fcdb2f2e81664fb2c46ee157b9f96
Carlos Saenz <carlos.saenz.g99@gmail.com>
2022-03-03 00:20:32 -0500

    chore: Creating form for sign up component

--- angular/03-admin-pro/src/app/auth/register/register.component.ts
+++ angular/03-admin-pro/src/app/auth/register/register.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: [ './register.component.css' ]
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  public registerForm = this.fb.group({
+    name: ['Carls', [Validators.required, Validators.minLength(2)]],
+    email: ['test100@test.com', [Validators.required]],
+    password: ['123456', [Validators.required]],
+    password2: ['123456', [Validators.required]],
+    terms: [false, [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  createUser() {
+    console.log(this.registerForm.value);
   }
-
 }
