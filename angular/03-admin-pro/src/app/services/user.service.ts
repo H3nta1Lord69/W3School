5c47264d64fde16ac9a4379ffe3dbe7b6afaea26
Carlos Garcia Saenz <carlos.saenz.g99@gmail.com>
2022-04-01 14:39:15 -0500

    chore: Setting token in local storage

--- angular/03-admin-pro/src/app/services/user.service.ts
+++ angular/03-admin-pro/src/app/services/user.service.ts
@@ -1,9 +1,13 @@
+// Angular importations
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
-//
-import { RegisterForm } from '../interfaces/register-form.interface';
+// Environmental imports
 import { environment } from 'src/environments/environment';
+
+// Models imports
+import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url;
@@ -15,10 +19,18 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   createUser(formData: RegisterForm) {
-    return this.http.post(`${base_url}/users`, formData);
+    return this.http.post(`${base_url}/users`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp['token']);
+      })
+    );
   }
 
   login(formData: LoginForm) {
-    return this.http.post(`${base_url}/login`, formData);
+    return this.http.post(`${base_url}/login`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp['token']);
+      })
+    );
   }
 }
