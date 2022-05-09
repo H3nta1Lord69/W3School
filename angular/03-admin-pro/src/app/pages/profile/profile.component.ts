41f13b5134aaa767e29e4565a8953a0f9ad05234
Carlos Saenz <cgarcia@wearenovae.com>
2022-05-08 23:04:22 -0500

    chore: Reactive load when the user is updated

--- angular/03-admin-pro/src/app/pages/profile/profile.component.ts
+++ angular/03-admin-pro/src/app/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -9,20 +10,25 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class ProfileComponent implements OnInit {
   profileForm!: FormGroup;
+  user?: User;
 
-  constructor(private fb: FormBuilder, private userService: UserService) {}
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.user = userService.user;
+  }
 
   ngOnInit(): void {
     this.profileForm = this.fb.group({
-      name: ['132', Validators.required],
-      email: ['ads', [Validators.required, Validators.email]],
+      name: [this.user?.name, Validators.required],
+      email: [this.user?.email, [Validators.required, Validators.email]],
     });
   }
 
   updateProfile() {
     console.log(this.profileForm?.value);
     this.userService.updateProfile(this.profileForm.value).subscribe((resp) => {
-      console.log(resp);
+      const { name, email } = this.profileForm.value;
+      this.user!.name = name;
+      this.user!.email = email;
     });
   }
 }
