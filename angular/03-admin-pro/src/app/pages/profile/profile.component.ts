bc86e94c5eda2aaf03ecf2e8cd2b87506eb5af86
Carlos Saenz <cgarcia@wearenovae.com>
2022-05-19 23:07:08 -0500

    chore: Update profile image

--- angular/03-admin-pro/src/app/pages/profile/profile.component.ts
+++ angular/03-admin-pro/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
+import { FileUploadService } from 'src/app/services/file-upload.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -11,8 +12,13 @@ import { UserService } from 'src/app/services/user.service';
 export class ProfileComponent implements OnInit {
   profileForm!: FormGroup;
   user?: User;
+  uploadImage?: File;
 
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private fileUpload: FileUploadService
+  ) {
     this.user = userService.user;
   }
 
@@ -30,5 +36,17 @@ export class ProfileComponent implements OnInit {
       this.user!.name = name;
       this.user!.email = email;
     });
+  }
+
+  changeImage(ev: any) {
+    this.uploadImage = ev.target.files[0];
+  }
+
+  imageUpload() {
+    this.fileUpload.updatePhoto(
+      this.uploadImage as File,
+      'users',
+      this.user?.uid as string
+    ).then;
   }
 }
